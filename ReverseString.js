1cb8269df1cf89d70d38ca3cac7d6b6d472a3738
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-02-17 19:01:27 -0500

    option one

--- ReverseString.js
+++ ReverseString.js
@@ -1,5 +1,13 @@
+// function reverseString(str){
+//     return str.split('').reverse().join('');
+// }
 function reverseString(str){
-    return str.split('').reverse().join('');;
+    let revString='';
+for (let i = str.length -1; i >= 0; i--) {
+     revString = revString + str[i];
+    
+}
+    return revString;
 }
 
 const output = reverseString('Hellow');
